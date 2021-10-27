e07d2d47d682e3918a0b7a74ad8f55e280ad81c6
SukhVirdiADA <sukh.virdi@ada.ac.uk>
2021-10-27 17:06:16 +0100

    editing endpoints

--- backend/routes/Quiz.js
+++ backend/routes/Quiz.js
@@ -8,6 +8,7 @@ const Quiz = require("../models/Quiz");
 userRouter.post("/add", passport.authenticate("jwt", {
 	session: false
 }), (req, res) => {
+
 	const quizTitle = req.body.quizTitle;
 	const quizQuestion = req.body.quizQuestion;
 	const answers = req.body.answers;
@@ -18,6 +19,7 @@ userRouter.post("/add", passport.authenticate("jwt", {
 		answers,
 		correctAnswer,
 	});
+	
 	newQuiz.save().then(() => res.json("Quiz Added!")).catch((err) => res.status(400).json({
 		message: {
 			messageBody: "ERROR:" + err,
@@ -41,39 +43,6 @@ userRouter.get("/:id", passport.authenticate("jwt", {
 	session: false
 }), (req, res) => {
 	Quiz.findById(req.params.id).then((quiz) => res.json(quiz)).catch((err) => res.status(400).json({
-		message: {
-			messageBody: "ERROR:" + err,
-			messageError: true
-		},
-	}));
-});
-
-userRouter.delete("/:id", passport.authenticate("jwt", {
-	session: false
-}), (req, res) => {
-	Quiz.findByIdAndDelete(req.params.id).then((quiz) => res.json("Quiz deleted!")).catch((err) => res.status(400).json({
-		message: {
-			messageBody: "ERROR:" + err,
-			messageError: true
-		},
-	}));
-});
-
-userRouter.post("/update/:id", passport.authenticate("jwt", {
-	session: false
-}), (req, res) => {
-	Quiz.findById(req.params.id).then((quiz) => {
-		quiz.quizTitle = req.body.quizTitle;
-		quiz.quizQuestion = req.body.quizQuestion;
-		quiz.answers = req.body.answers;
-		quiz.correctAnswer = req.body.correctAnswer;
-		quiz.save().then(() => res.json("Quiz updated!")).catch((err) => res.status(400).json({
-			message: {
-				messageBody: "ERROR:" + err,
-				messageError: true
-			},
-		}));
-	}).catch((err) => res.status(400).json({
 		message: {
 			messageBody: "ERROR:" + err,
 			messageError: true
